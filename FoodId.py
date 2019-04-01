ba264fdb32b3e01d04cc7a40dda5fbf8197b080e
Nigel Waterhouse <Nigel.h.waterhouse@gmail.com>
2019-03-31 22:57:53 -0400

    Main menu gui

--- FoodId.py
+++ FoodId.py
@@ -1,7 +1,18 @@
-import kivyimport kivy
+import kivy
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty, StringProperty
 from kivy.uix import *
 
 Builder.load_file('FoodId.kv')
+
+
+
+from ScreenManager import sm as ScreenManager
+
+class FoodIdApp(App):
+    def build(self):
+        return ScreenManager
+
+if __name__ == '__main__':
+    FoodIdApp().run()
